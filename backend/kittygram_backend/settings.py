0bbdbba274cabe84872a260de80ac5f7aa1a73bb
ToNyxRaZZoR <tonyxrazzor@yandex.ru>
2023-07-21 20:54:39 +0300

    Review oNe

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('TOKEN', 'SECRET')
 
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = bool(strtobool(os.getenv('DEBUG', 'false')))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
