75b637f1fb135f87372e3a12d538250325104090
ToNyxRaZZoR <tonyxrazzor@yandex.ru>
2023-07-23 02:53:25 +0300

    Dead Prod

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,11 +10,13 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('TOKEN', 'SECRET')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = bool(strtobool(os.getenv('DEBUG', 'false')))
+DEBUG = True
+#DEBUG = bool(strtobool(os.getenv('DEBUG', 'false')))
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
+#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
