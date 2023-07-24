0a6259a9daf8951946099f81ac5153ae41fbd1fe
ToNyxRaZZoR <tonyxrazzor@yandex.ru>
2023-07-24 03:14:18 +0300

    Final Dash-1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,11 +10,17 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('TOKEN', 'SECRET')
+#SECRET_KEY = os.getenv('TOKEN', 'SECRET')
 
-DEBUG = bool(strtobool(os.getenv('DEBUG', 'false')))
+#DEBUG = bool(strtobool(os.getenv('DEBUG', 'false')))
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
+#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
+
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
